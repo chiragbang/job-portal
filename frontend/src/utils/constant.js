6ec16f8e749a78ebef17e2faa9f05a861eb400f1
Chirag Bang <chiragbang33@gmail.com>
2024-11-25 08:56:21 +0000

    gitpod-test

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,1 +1,3 @@
-export const USER_API_END_POINT = "http://localhost:5000/api/v1/user"
+// export const USER_API_END_POINT = "http://localhost:5000/api/v1/user"
+
+export const USER_API_END_POINT = "https://3001-chiragbang-jobportal-1r2ru5p7j8i.ws-us116.gitpod.io/api/v1/user"
